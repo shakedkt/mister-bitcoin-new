f65a59cded9b17aee364ae54881ce19272ba4644
Shaked <shaked@7sjof9ku9h.lan>
2020-05-31 12:44:20 +0300

    hamburger

--- src/store/reducers/UserReducer.jsx
+++ src/store/reducers/UserReducer.jsx
@@ -1,23 +1,23 @@
 const initialState = {
-    users: [],
-    currUser: null
+  users: [],
+  currUser: null
+}
+
+export default function UserReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'SET_USERS':
+      return { ...state, users: action.users }
+    case 'SET_CURR_USER':
+      return { ...state, currUser: action.user }
+    case 'UPDATE_USER':
+      return {
+        ...state,
+        users: state.users.map(user => {
+          if (user._id === action.user._id) return action.user;
+          return user;
+        })
+      }
+    default:
+      return state;
   }
-  
-  export default function UserReducer(state = initialState, action) {
-    switch (action.type) {
-      case 'SET_USERS':
-        return { ...state, users: action.users }
-      case 'SET_CURR_USER':
-        return { ...state, currUser: action.user }
-      case 'UPDATE_USER':
-        return {
-          ...state,
-          users: state.users.map(user => {
-            if (user._id === action.user._id) return action.user;
-            return user;
-          })
-        }
-      default:
-        return state;
-    }
-  };
+};
